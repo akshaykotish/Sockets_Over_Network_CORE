fe248535e3b2f4b87afef039462e45711113e9bc
Akshay Kotish <akshaykotish@gmail.com>
2020-07-31 10:31:18 +0530

    Socket Network AWS Static IP

---
+++ TCP_Client/TCP_Client/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace TCP_Client
+{
+    class Program
+    {
+        private TcpClient client;
+        Thread client_process;
+        NetworkStream stream;
+        int port = 5005;
+        IPAddress ip;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            string ipstr = "192.168.43.43";
+            program.Start();
+            program.ip = IPAddress.Parse(ipstr);
+            program.IP(program.ip);
+            Console.WriteLine(ipstr);
+            Console.ReadKey();
+        }
+
+        private IPAddress GetExternalIPAddress()
+        {
+            IPHostEntry myIPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress myIPAddress in myIPHostEntry.AddressList)
+            {
+                byte[] ipBytes = myIPAddress.GetAddressBytes();
+
+                if (myIPAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    if (!IsPrivateIP(myIPAddress))
+                    {
+                        return myIPAddress;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private bool IsPrivateIP(IPAddress myIPAddress)
+        {
+            if (myIPAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                byte[] ipBytes = myIPAddress.GetAddressBytes();
+
+                // 10.0.0.0/24 
+                if (ipBytes[0] == 10)
+                {
+                    return true;
+                }
+                // 172.16.0.0/16
+                else if (ipBytes[0] == 172 && ipBytes[1] == 16)
+                {
+                    return true;
+                }
+                // 192.168.0.0/16
+                else if (ipBytes[0] == 192 && ipBytes[1] == 168)
+                {
+                    return true;
+                }
+                // 169.254.0.0/16
+                else if (ipBytes[0] == 169 && ipBytes[1] == 254)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        private bool CompareIpAddress(IPAddress IPAddress1, IPAddress IPAddress2)
+        {
+            byte[] b1 = IPAddress1.GetAddressBytes();
+            byte[] b2 = IPAddress2.GetAddressBytes();
+
+            if (b1.Length == b2.Length)
+            {
+                for (int i = 0; i < b1.Length; ++i)
+                {
+                    if (b1[i] != b2[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        void Port(int value)
+        {
+            port = value;
+        }
+        
+        void IP(IPAddress value)
+        {
+            ip = value;
+        }
+
+        void Start()
+        {
+            client_process = new Thread(new ThreadStart(Client));
+            client_process.Start();
+        }
+
+        void Client()
+        {
+            client = new TcpClient();
+            client.Connect(ip, port);
+            String info = "Hello Server this is client " + ip;
+            Send(info);
+        }
+
+        void Send(String info)
+        {
+            stream = client.GetStream();
+            byte[] message = Encoding.ASCII.GetBytes(info);
+            stream.Write(message, 0, message.Length);
+            stream.Close();
+            Console.WriteLine("Message send to " + ip.MapToIPv4().ToString() + " sucessfully.");
+        }
+
+        void Close()
+        {
+            client.Close();
+        }
+    }
+}
---
+++ TCP_Server/TCP_Server/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace TCP_Server
+{
+    class Program
+    {
+        private TcpListener server;
+        Thread server_process;
+        int port = 8082;
+        bool isAvailable = true;
+        static void Main(string[] args)
+        { 
+            Program program = new Program();
+            program.GetIpAddressList(Dns.GetHostName());
+            program.Start();
+        }
+
+        void check_port()
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            foreach (TcpConnectionInformation tcpi in tcpConnInfoArray)
+            {
+                if (tcpi.LocalEndPoint.Port == port)
+                {
+                    isAvailable = false;
+                    break;
+                }
+            }
+
+        }
+
+        public void GetIpAddressList(String hostString)
+        {
+            try
+            {
+                // Get 'IPHostEntry' object containing information like host name, IP addresses, aliases for a host.
+                IPHostEntry hostInfo = Dns.GetHostByName(hostString);
+                Console.WriteLine("Host name : " + hostInfo.HostName);
+                Console.WriteLine("IP address List : ");
+                for (int index = 0; index < hostInfo.AddressList.Length; index++)
+                {
+                    Console.WriteLine(hostInfo.AddressList[index]);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+        }
+
+        void Port(int value)
+        {
+            port = value;
+        }
+
+        void Start()
+        {
+            server_process = new Thread(new ThreadStart(Server));
+            server_process.Start();
+        }
+
+        void Server()
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Any, port);
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection dear..\n");
+                    TcpClient client = server.AcceptTcpClient();
+                    Thread client_process = new Thread(new ParameterizedThreadStart(client_handler));
+                    client_process.Start(client);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+        }
+
+        private void client_handler(object client)
+        {
+            TcpClient mClient = (TcpClient)client;
+            NetworkStream stream = mClient.GetStream();
+            byte[] message = new byte[1024];
+            stream.Read(message, 0, message.Length);
+            Console.WriteLine("Message:- " + Encoding.ASCII.GetString(message));
+        }
+    }
+}
